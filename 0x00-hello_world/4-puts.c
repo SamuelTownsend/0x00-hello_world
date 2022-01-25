74333de15883665d7722a50d0f39eb198d5b3d6d
SamuelTownsend <samjaytee13@gmail.com>
2022-01-24 16:57:36 -0800

    C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - entry point
- * 
- * Return: Always 0
- */
-int main (void)
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
