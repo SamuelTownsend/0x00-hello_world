203e25c7ea78498778b93aa2bea46127325317f1
SamuelTownsend <samjaytee13@gmail.com>
2022-02-01 18:06:26 -0800

    corrections

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,6 +11,8 @@ void print_square(int size)
 	int x;
 	int y;
 
+	if (size > 0)
+	{
 	for (x = 0; x < size; x++)
 	{
 		for (y = 0; y < size; y++)
@@ -19,5 +21,9 @@ void print_square(int size)
 		}
 		_putchar('\n');
 	}
+	}
+	else
+	{
 	_putchar('\n');
+	}
 }
