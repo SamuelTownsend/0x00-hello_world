91cbea64bcd085e7b2b44ae8ccf8c911a108156c
SamuelTownsend <samjaytee13@gmail.com>
2022-02-15 20:47:04 -0800

    corrections

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,7 @@
 #include "main.h"
 /*
- * Printing argc and argv
- */
-int main(int argc, char *argv[])
+*/int main(int argc __attribute__((unused)), char *argv[])
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
-	return (0);
+printf("%s\n", argv[0]);
+return (0);
 }
