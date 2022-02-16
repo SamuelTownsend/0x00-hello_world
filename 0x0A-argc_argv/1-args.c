861fd06bd4644246932cab7824ba4d957eb1350a
SamuelTownsend <samjaytee13@gmail.com>
2022-02-15 20:49:15 -0800

    updating syntax

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,7 @@
 #include "main.h"
 /*
- *
- */
-int main(int argc, char *argv[])
+*/int main(int argc, char *argv[] __attribute__((unused)))
 {
-	(void)argv;
-	printf("%d\n", argc - 1);
-	return (0);
+printf("%d\n", argc - 1);
+return (0);
 }
