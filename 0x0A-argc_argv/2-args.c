162a3601667211c42489b233e2ca1fea712c883b
SamuelTownsend <samjaytee13@gmail.com>
2022-02-15 20:51:23 -0800

    corrections

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,19 +1,10 @@
 #include "main.h"
-
-/**
-* main - entry point
-* @argc: number of arguments
-* @argv: array of argument strings
-* Return: Always 0
+/*
 */
-
 int main(int argc, char *argv[])
 {
-	int x;
-
-	for (x = 0; x < argc; x++)
-	{
-		printf("%s\n", argv[x]);
-	}
-	return (0);
+int x;
+for (x = 0; x < argc; x++)
+printf("%s\n", argv[x]);
+return (0);
 }
