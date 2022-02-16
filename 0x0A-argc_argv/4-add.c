ce3bcf52787f56a936b63f502100f66adefc9950
SamuelTownsend <samjaytee13@gmail.com>
2022-02-15 20:56:26 -0800

    fixed arg

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
-* main - entry point
-* @argc: number of arguments
-* @argv: argument strings in an array
-* Return: If an error, 1. Otherwise always 0
-*/
+ * main - main function
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
@@ -18,19 +17,18 @@ int main(int argc, char *argv[])
 		printf("%d\n", sum);
 		return (0);
 	}
-		for (x = 1; x < argc; x++)
+	for (x = 1; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
 		{
-			for (y = 0; argv[x][y] != '\0'; y++)
+			if (argv[x][y] < '0' || argv[x][y] > '9')
 			{
-				if (argv[x][y] < '0' || argv[x][y] > '9')
-				{
 				printf("Error\n");
 				return (1);
-				}
 			}
-
-			sum += atoi(argv[x]);
 		}
-		printf("%d\n", sum);
-		return (0);
+		sum += atoi(argv[x]);
+	}
+	printf("%d\n", sum);
+return (0);
 }
