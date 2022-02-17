457b8cc3221fb997853650c5aae1260b51987017
SamuelTownsend <samjaytee13@gmail.com>
2022-02-16 20:08:18 -0800

    more corrections

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,50 +1,36 @@
 #include "main.h"
-#include <string.h>
-/**
-* str_concat - entry point
-* @s1: string 1
-* @s2: string 2
-* Return: Concatenated string
-*/
+#include <stdlib.h>
+/*
+*/char *str_concat(char *s1, char *s2)
 
-char *str_concat(char *s1, char *s2)
 {
-	char *ptr;
-	unsigned int x = 0;
-	int string, length;
-
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-
-	for (string = 0; s1[string] || s2[string]; string++)
-	{
-		length++;
-	}
-
-	ptr = malloc(sizeof(char) * length);
-
-	if (ptr == NULL)
-	{
-		return (NULL);
-	}
-	while (*s1)
-
-	{
-		ptr[x] = *s1;
-		x++;
-		s1++;
-	}
-	while (*s2)
-	{
-		ptr[x] = *s2;
-		x++;
-		s2++;
-	}
-	return (ptr);
+int i;
+char *x = NULL;
+char *as1 = s1;
+char *as2 = s2;
+if (as1 == NULL)
+as1 = "";
+if (as2 == NULL)
+as2 = "";
+x = as1;
+while (*x++)
+i++;
+x = as2;
+while (*x++)
+i++;
+i++;
+x = malloc(i * sizeof(char));
+if (x == NULL)
+return (NULL);
+i = 0;
+while (*as1)
+{
+x[i++] = *as1++;
+}
+while (*as2)
+{
+x[i++] = *as2++;
+}
+x[i] = *as2;
+return (x);
 }
