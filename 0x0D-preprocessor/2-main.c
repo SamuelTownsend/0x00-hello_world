891ef4a1ad67314cdb8058d7e922baabb0bcfd48
SamuelTownsend <samjaytee13@gmail.com>
2022-02-17 19:58:21 -0800

    more corrections

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-/*
- * Betty Style
- */
+
+/**
+* main - entry point
+* Return: Always 0
+*/
+
 int main(void)
 {
-		printf("%s\n", __FILE__);
-		return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
