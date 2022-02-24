9c021c1f97036406dce573823aef949e6c716b6e
SamuelTownsend <samjaytee13@gmail.com>
2022-02-24 12:37:17 -0800

    4 corrections

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,19 +1,64 @@
 #include "dog.h"
 
 /**
- * betty comment
- *
+ * new_dog - check the code
+ * @name: dogs name
+ * @owner: name of owner
+ * @age: age of dog
+ * Return: new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *new_dog;
+	int i, nameLength, ownerLength, len;	
+	if (name == NULL || owner == NULL)
+		{
+			return (NULL);
+		}
+		dog_t *new_dog;
+		new_dog = malloc(sizeof(dog_t));
 
-	new_dog = malloc(sizeof(struct dog));
-	if (new_dog == NULL)
+		for (i=0; name[i] !='\0'; i++)
+		{
+			nameLength = i;
+		}
+
+		for (i=0; owner[i] !='\0'; i++)
+		{
+			ownerLength = i;
+		}
+	
+
+		new_dog->name = malloc(nameLength * sizeof(char));
+		if (new_dog->name == NULL)
+		{
+		free (new_dog);
 		return (NULL);
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
-	return (new_dog);
+		}
+		new_dog->owner = malloc(ownerLength *sizeof(char));
+		if (new_dog->owner == NULL)
+		{
+		free (new_dog->name);
+		free (new_dog);
+		return (NULL);
+		}
+		
+		len = nameLength;
+		new_dog->age = age;
+		
+		for (i=0; i <= len; i++)
+		{
+		
+			new_dog->name[i] = name[i];
+		}
+	
+		
+		
+		for (i=0; i < len; i++)
+		{
+			 new_dog->owner[i] = owner[i];
+		}
+	
+
+		return (new_dog);
 }
