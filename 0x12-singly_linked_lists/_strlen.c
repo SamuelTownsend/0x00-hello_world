f7f62e4a3fb4f2c1a64f4a510098e7331da44dfd
SamuelTownsend <samjaytee13@gmail.com>
2022-03-07 18:49:06 -0800

    requirment check commit

---
+++ 0x12-singly_linked_lists/_strlen.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* _strlen - check the code
+* @s: pointer
+* Return: strlength
+*/
+
+int _strlen(char *b)
+{
+	int strlength;
+
+	for (strlength = 0; b[strlength] != '\0'; strlength++)
+	{
+		continue;
+	}
+	return (strlength);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,64 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* struct list_s - singly linked list
+* @str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+*/
+
+typedef struct  list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+void free_list(list_t *head);
+list_t *add_node_end(list_t **head, const char *str);
+int _strlen(char *s);
+list_t *add_node(list_t **head, const char *str);
+size_t list_len(const list_t *h);
+size_t print_list(const list_t *h);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+
+#endif /* LISTS_H */
